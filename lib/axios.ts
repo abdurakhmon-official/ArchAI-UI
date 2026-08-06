787fc37f324f48dfa7cf4d80cb27dc176d3b52ac
abdurahmon <abdurahmongit1721@gmail.com>
2026-08-06 11:13:29 +0900

    toast

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Cookie from "js-cookie"
+import { toast } from "sonner"
 import {store} from "@/store/store"
 import {logout} from "@/store/slices/authSlice"
 
@@ -12,18 +13,29 @@ api.interceptors.request.use((config) => {
     if (token) {
         config.headers.Authorization = `Bearer ${token}`
     }
-    
+
     return config
 })
 
 api.interceptors.response.use(
-    (response) => response,
+    (response) => {
+        const message = response.data?._message
+        if (message) {
+            toast.success(message)
+        }
+        return response
+    },
     (error) => {
         if (error.response?.status === 401) {
             Cookie.remove("token")
             store.dispatch(logout())
+            toast.error("Session expired. Please sign in again.")
             window.location.href = "/login"
+            return Promise.reject(error)
         }
+
+        const message = error.response?.data?._message ?? error.message ?? "Something went wrong"
+        toast.error(message)
         return Promise.reject(error)
     }
 )
